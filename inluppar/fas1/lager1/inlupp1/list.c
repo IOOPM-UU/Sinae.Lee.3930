63ba60700e272fd25b1b66e0e0dd20421cf0fad0
Sinae Lee <Sinae@student93-10.eduroam.uu.se>
2016-09-19 12:21:19 +0200

    förbättrad list.c i

--- inluppar/fas1/lager1/inlupp1/list.c
+++ inluppar/fas1/lager1/inlupp1/list.c
@@ -7,9 +7,6 @@
 # include "list.h"
 
 
-//typedef struct tree tree_t; in header file
-//typedef struct list_list_t; in header file
-
 
 /// !! OBS -- du kommer att behöva ändra lite i denna fil. 
 /// !! Detta är en lista som endast hanterar heltal. Men det 
@@ -17,43 +14,41 @@
 /// !! andra data (vilka??) i vår (enda) lista. 
 
 
-/// Define struct list in your .c file not here! (why?)
-typedef struct list list_t;
-typedef struct node node_t;
 
-struct list{              //själva listan
+struct list{             
 
-  node_t *first;
-  node_t *last;
+  struct node *first;
+  struct node *last;
 };
 
-struct node{                   //länkarna
+struct node
+{                  
   void *elem;        // elem stored in the node. void * we can store whatever we like
-  node_t *next;     // pointer pointing to the next node in the list(for linked list), null for last node
+  struct node *next;     // pointer pointing to the next node in the list(for linked list), null for last node
 };
 
+struct node  *node_new(void *element, struct node *next)
+{
   
+  struct node *n = malloc(sizeof(struct node));
+  if (n == NULL)
+    return NULL;
+  n->elem = element;
+  n->next = next;      
+  return n;
+}
+
 /// Creates a new list
 ///
 /// \returns: empty list
 list_t *list_new()
 {
-  list_t *plist = malloc(sizeof(struct list)); //plist is pointer
-  if (plist == NULL)
+  list_t *l = malloc(sizeof(struct list)); 
+  if (l == NULL)
     return NULL;
-  plist->first = NULL;
-  plist->last =NULL;
-  return plist;
-}
-
-node_t *node_new(void *element, node_t *next)
-{
-  node_t *pnode = malloc(sizeof(struct node));
-  if (pnode == NULL)
-    return NULL;
-  pnode->elem = element;
-  pnode->next = next;
-  return pnode;
+  l->first = NULL;
+  l->last = NULL;
+  return l;
 }
 
 /// Inserts a new element at the end of the list
@@ -62,28 +57,31 @@ node_t *node_new(void *element, node_t *next)
 /// \param elem the integer to be appended
 void list_append(list_t *list, int elem)
 {
-  node_t *newElem = node_new(elem, list);
+  struct node *newElem = node_new(&elem, list->last);
   
   if (list == NULL)
     {
-      list->first = newElem;
+     list->last = list->first = newElem;
     }
   else
     {
+      list->last->next = newElem;
       list->last = newElem;
-      list->last->next = NULL;
     }
 }
 
-/// Inserts a new element at the head of the list
+/// Inserts a new element at the beginning of the list
 ///
 /// \param list pointer to the list
 /// \param elem the integer to be prepended
 void list_prepend(list_t *list, int elem)
 {
-  node_t *newElem = node_new(elem, list);
+  struct node *newElem = node_new(&elem, list->first);
+  
   if (list == NULL)
-    list->last = list->first = newElem;
+    {
+      list->first = list->last = newElem;
+    }
 
   else
     {
@@ -92,69 +90,28 @@ void list_prepend(list_t *list, int elem)
   
 }
 
-/// Inserts a new element at a given index. 
-///
-/// Valid indexes are [0..size]. 
-///
-/// Example:
-///
-/// list_t *l = list_new(); // l == []
-/// list_insert(l, 0, 42);  // l == [42]
-/// list_insert(l, 0, 43);  // l == [43, 42]
-/// list_insert(l, 1, 44);  // l == [43, 44, 42]
-/// list_insert(l, 5, 45);  // l == [43, 44, 42]
-///
-/// The last case fails (and returns false) because
-/// size is 3, meaning 5 is not a valid index. 
-///
-/// Note that insert at index 0 is the same as prepend 
-/// and insert index size is the same as append. 
-///
-/// Negative indexes should be supported:
-///
-/// list_insert(l, -1, 45);       // l == [43, 44, 42, 45]
-///
-/// A positive index can be calculated from a 
-/// negative like this: pos_i = size + 1 + neg_i.
-/// 
-/// \param list  pointer to the list
-/// \param index the index for elem to be inserted at
-/// \param elem  the integer to be prepended
-/// \returns true if succeeded, else false
-bool list_insert(list_t *list, int index, int elem);
-
-/// Removes an element from a list.
-///
-/// Example: (assume l == [43, 44, 42, 45])
-///
-/// int elem;
-/// list_remove(l, 1, &elem);  // l = [43, 42, 45], elem == 44
-/// list_remove(l, -1, &elem); // l = [43, 42], elem == 45
-///
-/// \param list  pointer to the list
-/// \param index the index to be removed
-/// \param elem a pointer to where the element can be stored
-/// \returns true if succeeded, else 
-bool list_remove(list_t *list, int index, int *elem);
-
-/// Returns the element at a given index
-/// \param list  pointer to the list
-/// \param index the index to be returns
-/// \returns a pointer to the element at index index
-int *list_get(list_t *list, int index);
 
 /// A convenience for list_get(list, 0)
-int *list_first(list_t *list);
+int *list_first(list_t *list)
+{
+  struct node *f_cursor = list->first;
+
+  if (f_cursor == NULL)
+    return NULL;
+
+  else
+    return f_cursor->elem;
+
+}
 
 /// A convenience for list_get(list, -1)
-int *list_last(list_t *list);
+int *list_last(list_t *list)
+{
+  struct node *l_cursor = list->last;
 
-/// Returns the length of the list. It is undefined
-/// whether the length is calculated in O(n) time or
-/// whether there is a size counter in the list object
-/// that is manipulated by insert, remove, etc. 
-/// \param list the list
-/// \returns the length of list
-int list_length(list_t *list);
+  if (l_cursor == NULL)
+    return NULL;
 
-
+  else
+    return l_cursor->elem;
+}
