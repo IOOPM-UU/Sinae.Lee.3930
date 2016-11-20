026572399fd69ae73d7263a2da8ec29d13e041d8
Sinae Lee <Sinae@Sinaes-MacBook-Pro.local>
2016-11-20 11:02:23 +0100

    update

--- inluppar/fas2/kassako/Simulator.java
+++ inluppar/fas2/kassako/Simulator.java
@@ -3,11 +3,11 @@ public class Simulator{
     public static void main(String[] args) throws InterruptedException{
 
         int steps = 100;
-        Customer q = new Customer(0, 3);
+        
         
         Simulation s = new Simulation(3, 50, 5, 4); // TODO: Add parameters!
 
-        s.store.newCustomer(q);
+       
         for(int i = 0; i < steps; i++){
             System.out.print("\033[2J\033[;H");
             s.step();
