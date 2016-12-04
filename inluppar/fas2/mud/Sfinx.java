30e22516fe354adb755cb6e4fa20e185bac3e590
Sinae Lee <sinaelee@me.com>
2016-12-04 17:58:03 +0100

    Delete Sfinx.java

--- inluppar/fas2/mud/Sfinx.java
+++
@@ -1,9 +0,0 @@
-public class Sfinx{
-    private LinkedList<str> wisdom;
-    private Diplom diplom;
-
-    public Sfinx(){
-        this.wisdom = new LinkedList<str>();
-        this.diplom = new Diplom();
-    }
-}
